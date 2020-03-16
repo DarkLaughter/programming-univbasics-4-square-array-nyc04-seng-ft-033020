672dc1091feb10bb6ecea31fef270062853cdf61
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-16 04:04:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  squared = []
+
+  while counter <= array.length
+    square = array[counter] * array[counter]
+    squared.push(square)
+
+    counter += 1
+  end
+  squared
 end
